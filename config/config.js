9c01b7e23ea2e8b554622640a1b504e950462ae6
jecs <jesus.calderon2708@gmail.com>
2021-10-07 20:25:01 -0600

    se agrega archivo database || se agrega uso de cors

--- config/config.js
+++ config/config.js
@@ -7,4 +7,4 @@ module.exports = {
     dbHost: process.env.DBHOST || 'kashin.db.elephantsql.com',
     dbDialect: process.env.DIALECT || 'postgres',
     token_key: process.env.SECRET_TOKEN || '6Zfx~^AFX8o3"<l,!S*B#kM>6:p:.q).(KKH;k>4ConY7wp.yd^g@^g#PvsK*I:'
-}
+} 
