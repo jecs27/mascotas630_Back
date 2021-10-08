57a555f8d561a4fd72687c3c6e8fa8c50372b4fe
jecs <jesus.calderon2708@gmail.com>
2021-10-07 20:48:11 -0600

    se agrega modelo de usuarios

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+let db = require('./models/database');
+
+async function createDB() {
+    await db.sequelize
+        .sync({ alter: true, force: false })
+        .then(() => {
+            return Promise.resolve();
+        })
+        .catch(err => {
+            return Promise.reject(err);
+        });
+}
+
+createDB();
 
 var app = express();
 app.use(cors());
@@ -14,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 module.exports = app;
---
+++ models/users.js
@@ -0,0 +1,50 @@
+'use strict';
+const moment = require("moment");
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Users extends Model {
+        static associate(models) {}
+    }
+    Users.init({
+        create_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: moment().format('YYYY-MM-DD'),
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrementIdentity: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING(51),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            defaultValue: ''
+        },
+        uuid: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        status: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+        },
+    }, {
+        sequelize,
+        timestamps: false,
+        indexes: [
+            { fields: ['user_id'], unique: true }
+        ],
+        modelName: 'Users',
+        freezeTableName: true
+    });
+    return Users;
+};
